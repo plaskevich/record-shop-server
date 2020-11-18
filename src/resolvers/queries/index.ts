fa410b3137d3bfe07129576eaa81da5090e87d84
Alex Plaskevich <45790929+plaskevich@users.noreply.github.com>
2020-11-18 17:11:30 +0100

    add genre statistics

--- src/resolvers/queries/index.ts
+++ src/resolvers/queries/index.ts
@@ -1,4 +1,5 @@
 import { RecordModel } from '../../models/Record';
+import { ShopModel } from '../../models/Shop';
 
 export async function getRecord(
   _: any,
@@ -35,4 +36,26 @@ export async function getSoldRecords(_: any, data: any, { currentUser }: any) {
   });
   if (!records) return new Error('Records not found');
   return records;
+}
+
+export async function getShop(_: any, { id }: { id : string }) {
+  const shop = await ShopModel.findById(id);
+  return shop
+}
+
+export async function getGenreStatistics (_:any, data: any, {currentUser} : any) {
+  if (!currentUser) throw new Error('Invalid user');
+  const records = await RecordModel.find({ shop: currentUser.shop });
+  const result: { genre: string; records: string[]; percent: number }[] = [];
+  const genreObject = records.reduce(function (r, a) {
+    r[a.genre] = r[a.genre] || [];
+    r[a.genre].push(a.id);
+    return r;
+}, Object.create(null));
+
+  Object.keys(genreObject).forEach(e => {
+    const percent = 100/(records.length/genreObject[e].length)
+    result.push({ genre: e, records: genreObject[e], percent})
+  })
+  return result
 }
