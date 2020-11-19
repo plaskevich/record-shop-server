682c8d5e0ddb1f52f7bc0318773fc7cc3a25674a
Alex Plaskevich <45790929+plaskevich@users.noreply.github.com>
2020-11-19 21:34:54 +0100

    update modules and fix authorization

--- src/resolvers/mutations/index.ts
+++ src/resolvers/mutations/index.ts
@@ -5,22 +5,22 @@ import { mongoose } from '@typegoose/typegoose';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
-export async function signUp(_: any, { data }: any) {
-  const hashPass = await bcrypt.hash(data.password, 12);
+export async function signUp(_: any, { email, password, name }: { email: string, password: string, name: string }) {
+  const hashPass = await bcrypt.hash(password, 12);
   const newUser = new UserModel({
-    email: data.email,
+    email: email,
     password: hashPass,
-    name: data.name,
+    name: name,
   });
   await newUser.save();
   const token = jwt.sign({ userId: newUser.id }, 'secret');
   return { token, user: newUser };
 }
 
-export async function signIn(_: any, { data }: any) {
-  const user = await UserModel.findOne({ email: data.email });
+export async function signIn(_: any, { email, password }: { email: string, password: string }) {
+  const user = await UserModel.findOne({ email: email });
   if (!user) throw new Error('User was not found');
-  const compare = await bcrypt.compare(data.password, user.password);
+  const compare = await bcrypt.compare(password, user.password);
   if (!compare) throw new Error('Incorrect credentials');
   const token = jwt.sign({ userId: user.id }, 'secret');
   return { token, user };
