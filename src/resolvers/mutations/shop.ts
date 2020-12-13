dfd96463f77aa4670d7114fdcbffe97850b6c794
Alex Plaskevich <45790929+plaskevich@users.noreply.github.com>
2020-12-13 19:22:31 +0100

    wip

--- src/resolvers/mutations/shop.ts
+++ src/resolvers/mutations/shop.ts
@@ -2,34 +2,34 @@ import { UserModel } from '../../models/User';
 import { ShopModel } from '../../models/Shop';
 
 export async function addShop(_: any, { name }: { name: string }) {
-    const newShop = new ShopModel({ name });
-    await newShop.save();
-    return newShop;
-  }
-  
-  export async function addUserToShop(_: any, { shopId, userId, role }: { shopId: string, userId: string, role: string }, { currentUser }: any) {
-    const shop = await ShopModel.findById(shopId)
-    if (shop?.users.some(item => item.userId === currentUser.id && item.role === 'admin')){
-      if (shop?.users.some(item => item.userId === userId)) {
-        throw new Error ('User already added')
-      } else {
-        const user = await UserModel.findById(userId)
-        if (!user) throw new Error ('User you are trying to add does not exist')
-        shop?.users.push({ userId, role})
-        return await shop?.save()
-      }
+  const newShop = new ShopModel({ name });
+  await newShop.save();
+  return newShop;
+}
+
+export async function addUserToShop(_: any, { shopId, userId, role }: { shopId: string, userId: string, role: string }, { currentUser }: any) {
+  const shop = await ShopModel.findById(shopId)
+  if (shop?.users.some(item => item.userId === currentUser.id && item.role === 'admin')) {
+    if (shop?.users.some(item => item.userId === userId)) {
+      throw new Error('User already added')
     } else {
-      throw new Error ("You don't have permisson for this operation")
+      const user = await UserModel.findById(userId)
+      if (!user) throw new Error('User you are trying to add does not exist')
+      shop?.users.push({ userId, role })
+      return await shop?.save()
     }
+  } else {
+    throw new Error("You don't have permisson for this operation")
   }
-  
-  export async function changeUserRole(_: any, { shopId, userId, role }: { shopId: string, userId: string, role: string }, { currentUser }: any) {
-    const shop = await ShopModel.findById(shopId)
-    if (shop?.users.some(item => item.userId === currentUser.id && item.role === 'admin')){
-        return await shop.updateOne({'users.userId': userId}, {'$set': {
-             'users.$.role': role
-         }}, function(err) { console.log(err) })
-    } else {
-      throw new Error ("You don't have permisson for this operation")
-    }
-  }
+}
+
+export async function changeUserRole(_: any, { shopId, userId, role }: { shopId: string, userId: string, role: string }, { currentUser }: any) {
+  const shop = await ShopModel.findById(shopId)
+  // if (shop?.users.some(item => item.userId === currentUser.id && item.role === 'admin')){
+  //     return await shop.updateOne({'users.userId': userId}, {'$set': {
+  //          'users.$.role': role
+  //      }}, function(err) { console.log(err) })
+  // } else {
+  //   throw new Error ("You don't have permisson for this operation")
+  // }
+}
